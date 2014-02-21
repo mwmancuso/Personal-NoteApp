d51bc04df50205fac4cfb38105aef39635cf000d
Matt Mancuso <xpandismo@gmail.com>
2014-02-21 15:58:12 -0500

    Fixed server-specific files.

--- Notesapp/settings.py
+++ Notesapp/settings.py
@@ -76,4 +76,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/notes.xpandismo.tk/static/'
+STATIC_ROOT = '/var/www/notes.xpandismo.com/static/'

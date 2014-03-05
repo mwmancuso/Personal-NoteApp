b79e5331cbeb5306e1b912027c10adfec21ce388
Matt Mancuso <xpandismo@gmail.com>
2014-03-05 17:01:22 -0500

    Cleaned up some code.

--- Notesapp/routers.py
+++ Notesapp/routers.py
@@ -1,30 +1,30 @@
 # Define database definitions to use for specific apps
 app_databases = {
-	'authentication': {
-		'rw':   'authentication',
-		'ro':   'authentication_ro'
-	}
+    'authentication': {
+        'rw':   'authentication',
+        'ro':   'authentication_ro'
+    }
 }
 
 class AppRouter(object):
-	"""
-	Router that routes specific apps to specific database definitions if defined.
-	"""
-	def db_for_read(self, model, **hints):
-		"""
-		Routes read only calls to defined read only database definition.
-		"""
-		if model._meta.app_label in app_databases:
-			return app_databases[model._meta.app_label]['ro']
-		else:
-			return 'default'
-	
-	
-	def db_for_write(self, model, **hints):
-		"""
-		Routes write calls to defined read/write database definition.
-		"""
-		if model._meta.app_label in app_databases:
-			return app_databases[model._meta.app_label]['rw']
-		else:
-			return 'default'
+    """
+    Router that routes specific apps to specific database definitions if defined.
+    """
+    def db_for_read(self, model, **hints):
+        """
+        Routes read only calls to defined read only database definition.
+        """
+        if model._meta.app_label in app_databases:
+            return app_databases[model._meta.app_label]['ro']
+        else:
+            return 'default'
+    
+    
+    def db_for_write(self, model, **hints):
+        """
+        Routes write calls to defined read/write database definition.
+        """
+        if model._meta.app_label in app_databases:
+            return app_databases[model._meta.app_label]['rw']
+        else:
+            return 'default'
--- authentication/models.py
+++ authentication/models.py
@@ -1,20 +1,26 @@
 from django.db import models
 
 class Users(models.Model):
-	username = models.CharField(max_length=50, unique=True)
-	first_name = models.CharField(max_length=30, blank=True)
-	last_name = models.CharField(max_length=30, blank=True)
-	email = models.EmailField(max_length=75)
-	user_type = models.IntegerField(default=0)
-	last_access = models.DateTimeField()
-	created = models.DateTimeField(auto_now_add=True)
-	modified = models.DateTimeField(auto_now=True, auto_now_add=True)
+    username = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    email = models.EmailField(max_length=75)
+    user_type = models.IntegerField(default=0)
+    last_access = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True, auto_now_add=True)
+    
+    def __str__(self):
+        return self.username
 
 class Methods(models.Model):
-	user_id = models.ForeignKey(Users)
-	method = models.IntegerField()
-	password = models.CharField(max_length=60, blank=True)
-	token = models.TextField(blank=True)
-	step = models.IntegerField()
-	status = models.IntegerField(default=0)
-	updated = models.DateTimeField(auto_now=True, auto_now_add=True)
+    user_id = models.ForeignKey(Users)
+    method = models.IntegerField()
+    password = models.CharField(max_length=60, blank=True)
+    token = models.TextField(blank=True)
+    step = models.IntegerField()
+    status = models.IntegerField(default=0)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=True)
+    
+    def __str__(self):
+        return self.user_id

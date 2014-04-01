daeec5ee99222806c37c8f7958b39e24cfbaa7b3
Matt Mancuso <xpandismo@gmail.com>
2014-04-01 19:23:30 -0400

    Updated meta model with automatic tag population.

--- meta/models.py
+++ meta/models.py
@@ -1,5 +1,27 @@
 from django.db import models
 
+DEFAULT_DATA = {
+    'new-users': {
+        # 1 for yes, 0 for no
+        'setting': 1,
+        'data': '',
+    },
+    'user-login': {
+        # 1 for yes, 0 for no
+        'setting': 1,
+        'data': '',
+    },
+}
+
+class DataManager(models.Manager):
+    """Manager for meta-data class."""
+    
+    def populate(self):
+        """Populates database with data given in DEFAULT_DATA."""
+        
+        for tag in DEFAULT_DATA.keys():
+            object = self.get_or_create(tag=tag, defaults=DEFAULT_DATA[tag])
+
 class Data(models.Model):
     """Model for meta-data. Needed for system-wide key/value pairs.
     
@@ -16,10 +38,13 @@ class Data(models.Model):
     """
     
     tag = models.CharField(max_length=30, unique=True)
-    setting = models.IntegerField(blank=True)
+    setting = models.IntegerField(null=True)
     data = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True, auto_now_add=True)
+    
+    # Replaces default manager with custom one
+    objects = DataManager()
     
     def __str__(self):
         return self.tag
